67a7ca7bf96c60407216e50749199192f8e706d4
WilsonOuGig <wilson_ou@yahoo.ca>
2021-01-03 09:05:18 -0500

    Update Main.java
    refactored to end of i to a variable to avoid running Math.sqrt many times in to for loop.

--- PrettyAveragePrimes/Main.java
+++ PrettyAveragePrimes/Main.java
@@ -53,7 +53,9 @@ public class Main {
         if (number % 2 == 0) {
             return false;
         }
-        for (int i = 3; i <= Math.floor(Math.sqrt(number)); i = i + 2) {
+        
+        int end = Math.floor(Math.sqrt(number));
+        for (int i = 3; i <= end; i = i + 2) {
             if (number % i == 0) {
                 return false;
             }
