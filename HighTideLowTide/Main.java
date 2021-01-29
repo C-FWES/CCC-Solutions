036a663f87162f403a43e459d0caad0a6271bf53
Wesley <69126784+C-FWES@users.noreply.github.com>
2021-01-29 17:37:43 -0500

    High tides

---
+++ HighTideLowTide/Main.java
@@ -0,0 +1,44 @@
+package HighTideLowTide;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        String[] s = sc.nextLine().split("\\s");
+
+        Integer[] measurements = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            measurements[i] = Integer.parseInt(s[i]);
+        }
+        Arrays.sort(measurements);
+
+        int highLength = n/2;
+        int lowLength = n - highLength;
+
+//        Integer[] lowTides = Arrays.copyOfRange(measurements, 0, lowLength);
+//        Integer[] hightTides = Arrays.copyOfRange(measurements, lowLength, measurements.length);
+
+        LinkedList<Integer> arrangedMeasurements = new LinkedList<>();
+//        reverse(lowTides);
+        for (int i = lowLength - 1; i >= 0; i--) {
+            arrangedMeasurements.add(measurements[i]);
+            int j = 2*lowLength - i - 1;
+
+            if (j < measurements.length) {
+                arrangedMeasurements.add(measurements[j]);
+            }
+
+
+        }
+
+        for (Integer arrangedMeasurement : arrangedMeasurements) {
+            System.out.print(arrangedMeasurement + " ");
+        }
+
+
+
+    }
+
+}
